53518f1a3c60516a7233a3f864a9b6ed13da7b8b
Balram Pandey <balrampandey19@gmail.com>
2017-04-13 13:40:05 +0530

    code  fix

--- app.py
+++ app.py
@@ -36,37 +36,37 @@ def create_store():
     stores.append(new_store)
     return jsonify(new_store)
 
-@app.route("/store<string:name>")
+@app.route('/store/<string:name>')
 def get_store(name):
-    for store in stores:
-        if(store['name'] == name)
-        return jsonify(store)
-    return jsonify({'message':'Store not found'})
+  for store in stores:
+    if store['name'] == name:
+          return jsonify(store)
+  return jsonify ({'message': 'store not found'})
 
 @app.route("/store")
 def get_stores():
     return jsonify({'stores':stores})
 
-@app.route("/store<string:name>/item",methods=['POST'])
-def create_item_in_store():
-    request_data = request.get_json()
-    for store in stores:
-        if(store['name'] == name)
-        store_item={
-        'name':request_data['name'],
-        'price':request_data['price']
+@app.route('/store/<string:name>/item' , methods=['POST'])
+def create_item_in_store(name):
+  request_data = request.get_json()
+  for store in stores:
+    if store['name'] == name:
+        new_item = {
+            'name': request_data['name'],
+            'price': request_data['price']
         }
-        store['items'].append(store_item)
-        return jsonify(store_item)
-    return jsonify({'message':'Store not found'})
+        store['items'].append(new_item)
+        return jsonify(new_item)
+  return jsonify ({'message' :'store not found'})
 
 
-@app.route("/store<string:name>/item")
+@app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    for store in stores:
-        if(store['name'] == name)
-        return jsonify(store['items'])
-    return jsonify({'message':'Store not found'})
+  for store in stores:
+    if store['name'] == name:
+        return jsonify( {'items':store['items'] } )
+  return jsonify ({'message':'store not found'})
 
 
 app.run(port=5000)
